a4d8758eb1e0e62904d3dd741ea1b4aab54045fb
swimR <iva.petrovic5@gmail.com>
2016-02-18 00:41:15 +0100

    Hiding buttons based on question mode
    - if mode2_v button 3 and 4 are hidden
    - if mode2_h button 2 and 4 are hidden

--- Assets/NewGameManager.cs
+++ Assets/NewGameManager.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 public class NewGameManager : MonoBehaviour {
 
 	private Button answ;
+	private Button answer1;
 	private Button answer2;
 	private Button answer3;
 	private Button answer4;
@@ -15,34 +16,47 @@ public class NewGameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		questionSetManager = new QuestionSetManager ();
+
 		questionText = GameObject.Find ("question_text").GetComponent<Text> ();
-		/*answer1 = GameObject.Find ("answer1_button").GetComponent<Button> ();
+		answer1 = GameObject.Find ("answer1_button").GetComponent<Button> ();
 		answer2 = GameObject.Find ("answer2_button").GetComponent<Button> ();
 		answer3 = GameObject.Find ("answer3_button").GetComponent<Button> ();
-		answer4 = GameObject.Find ("answer4_button").GetComponent<Button> ();*/
+		answer4 = GameObject.Find ("answer4_button").GetComponent<Button> ();
+
 		string mode = "";
 		string question_text = "";
-		string answer1_text = "";
 
 		List<Question> questionList = questionSetManager.importQuestions ().getQuestionList ();
-		foreach (Question question in questionList) {
-			List<Answer> answerList = question.getAnswers ();
-			mode = question.getQuestionMode ().ToString();
-			question_text = question.getQuestionText ();
-			int i = 1;
-			foreach (Answer answer in answerList) {
-				string name = "answer" + i.ToString () + "_button"; 
-				answ = GameObject.Find (name).GetComponent<Button> ();
-				answ.GetComponentInChildren<Text>().text = answer.getText ();
-				Debug.Log (name);
-				i++;
-			}
-			Debug.Log (question_text);
-			break;
-		}
+		Question firstQuestion = questionList [0];
+		List<Answer> answerList = firstQuestion.getAnswers ();
+		mode = firstQuestion.getQuestionMode ().ToString();
+		question_text = firstQuestion.getQuestionText ();
 
 		questionText.text = question_text;
 
+		int i = 1;
+		foreach (Answer answer in answerList) {
+			if (mode == "MODE2_H") {
+				if (i == 1) {
+					answer1.GetComponentInChildren<Text> ().text = answer.getText ();
+				} else {
+					answer3.GetComponentInChildren<Text> ().text = answer.getText ();
+				}
+			} else {
+				string name = "answer" + i.ToString () + "_button"; 
+				answ = GameObject.Find (name).GetComponent<Button> ();
+				answ.GetComponentInChildren<Text>().text = answer.getText ();
+			}
+			i++;
+		}
+
+		if (mode == "MODE2_V") {
+			answer3.gameObject.SetActive (false);
+			answer4.gameObject.SetActive (false);
+		} else if (mode == "MODE2_H") {
+			answer2.gameObject.SetActive (false);
+			answer4.gameObject.SetActive (false);
+		}
 
 	}
 	
