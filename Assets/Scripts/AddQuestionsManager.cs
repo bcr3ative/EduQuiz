f385b48157f9995b536d7280159e34de84666f54
bcr3ative <paolo.perkovic@gmail.com>
2016-02-18 11:32:57 +0100

    Streamlined refactoring to keep it consistent
    - Changed named of few scenes and scripts
    - All scenes now have SceneManager GameObject for attaching scripts
    - Deleted unnecessary scenes
    - Moved NewGameManager script to appropriate folder
    - Added .gitignore for *.meta files

--- Assets/Scripts/AddQuestionsManager.cs
+++ Assets/Scripts/AddQuestionsManager.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class QuestionScene : MonoBehaviour {
+public class AddQuestionsManager : MonoBehaviour {
 
 	private InputField inputQuestion;
 	private InputField inputField1;
---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuManager: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void startGame() {
+		SceneManager.LoadScene("new_game");
+	}
+
+	public void addQuestions() {
+		SceneManager.LoadScene("add_questions");
+	}
+}
