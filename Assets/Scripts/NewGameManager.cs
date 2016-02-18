04788d07209f2d1ff4a7d1146753f07e8aad0488
bcr3ative <paolo.perkovic@gmail.com>
2016-02-18 13:27:24 +0100

    Fixed button translation/transformation on new game

--- Assets/Scripts/NewGameManager.cs
+++ Assets/Scripts/NewGameManager.cs
@@ -68,58 +68,67 @@ public class NewGameManager : MonoBehaviour {
 
 		// button resizing and positioning based on the question mode
 		if (mode == Question.QuestionType.MODE2_V) {
-			// set size of first button
-			answer1.image.rectTransform.sizeDelta = new Vector2 (478, 350);
-			//set position of first button
-			Vector3 temp = answer1.transform.position;
-			temp.x = 250;
-			temp.y = 220;
-			temp.z = 0;
-			answer1.transform.position = temp;
+			// create button size
+			Vector2 buttonSize = new Vector2 (430, 350);
 
-			// set size of second button
-			answer2.image.rectTransform.sizeDelta = new Vector2 (478, 350);
-			// set position of second button
-			temp = answer2.transform.position;
-			temp.x = 820;
-			temp.y = 220;
-			temp.z = 0;
-			answer2.transform.position = temp;
+			// set buttons 1 and 2 transformations and translations
+			answer1.image.rectTransform.sizeDelta = buttonSize;
+			answer1.transform.position = new Vector3(-215 + buttonSize.x, buttonSize.y/2, 0);
 
-			// hide third and fourth button 
-			answer3.gameObject.SetActive (false);
-			answer4.gameObject.SetActive (false);
+			answer2.image.rectTransform.sizeDelta = buttonSize;
+			answer2.transform.position = new Vector3(215 + buttonSize.x, buttonSize.y/2, 0);
+
+			// activate buttons 1 and 2
+			answer1.gameObject.SetActive(true);
+			answer2.gameObject.SetActive(true);
+			answer3.gameObject.SetActive(false);
+			answer4.gameObject.SetActive(false);
 
 			// set answers text
 			answer1.GetComponentInChildren<Text>().text = answers[0].getText();
 			answer2.GetComponentInChildren<Text>().text = answers[1].getText();
 		} else if (mode == Question.QuestionType.MODE2_H) {
-			// set size of first button
-			answer1.image.rectTransform.sizeDelta = new Vector2 (956, 175);
-			//set position of first button
-			Vector3 temp = answer1.transform.position;
-			temp.x = 500;
-			temp.y = 350;
-			temp.z = 0;
-			answer1.transform.position = temp;
+			// create button size
+			Vector2 buttonSize = new Vector2 (860, 175);
 
-			// set size of third button
-			answer3.image.rectTransform.sizeDelta = new Vector2 (956, 175);
-			// set position of second button
-			temp = answer2.transform.position;
-			temp.x = 500;
-			temp.y = 100;
-			temp.z = 0;
-			answer3.transform.position = temp;
+			// set buttons 1 and 3 transformations and translations
+			answer1.image.rectTransform.sizeDelta = buttonSize;
+			answer1.transform.position = new Vector3(buttonSize.x/2, 49.5f + buttonSize.y + 38, 0);
 
-			// hide second and fourth button 
-			answer2.gameObject.SetActive (false);
-			answer4.gameObject.SetActive (false);
+			answer3.image.rectTransform.sizeDelta = buttonSize;
+			answer3.transform.position = new Vector3(buttonSize.x/2, -125.5f + buttonSize.y + 38, 0);
+
+			// activate buttons 1 and 3
+			answer1.gameObject.SetActive(true);
+			answer3.gameObject.SetActive(true);
+			answer2.gameObject.SetActive(false);
+			answer4.gameObject.SetActive(false);
 
 			// set answers text
 			answer1.GetComponentInChildren<Text>().text = answers[0].getText();
 			answer3.GetComponentInChildren<Text>().text = answers[1].getText();
 		} else if (mode == Question.QuestionType.MODE4) {
+			// create button size
+			Vector2 buttonSize = new Vector2 (430, 175);
+
+			// set all four buttons transformations and translations
+			answer1.image.rectTransform.sizeDelta = buttonSize;
+			answer1.transform.position = new Vector3(-215 + buttonSize.x, 49.5f + buttonSize.y + 38, 0);
+
+			answer2.image.rectTransform.sizeDelta = buttonSize;
+			answer2.transform.position = new Vector3(215 + buttonSize.x, 49.5f + buttonSize.y + 38, 0);
+
+			answer3.image.rectTransform.sizeDelta = buttonSize;
+			answer3.transform.position = new Vector3(-215 + buttonSize.x, -125.5f + buttonSize.y + 38, 0);
+
+			answer4.image.rectTransform.sizeDelta = buttonSize;
+			answer4.transform.position = new Vector3(215 + buttonSize.x, -125.5f + buttonSize.y + 38, 0);
+
+			// activate all four buttons
+			answer1.gameObject.SetActive(true);
+			answer3.gameObject.SetActive(true);
+			answer2.gameObject.SetActive(true);
+			answer4.gameObject.SetActive(true);
 
 			// set answers text
 			answer1.GetComponentInChildren<Text>().text = answers[0].getText();
