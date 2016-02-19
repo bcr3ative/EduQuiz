b99a1455df88715c3735987c54f5e8455704e294
bcr3ative <paolo.perkovic@gmail.com>
2016-02-19 10:51:24 +0100

    Remove sound toggling, few other fixes

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -14,12 +14,12 @@ public class SettingsManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		sound = GameObject.Find("sound_toggle").GetComponent<Toggle>();
+		//sound = GameObject.Find("sound_toggle").GetComponent<Toggle>();
 		font_size = GameObject.Find("fontsize_input").GetComponent<InputField>();
 		time = GameObject.Find("time_input").GetComponent<InputField>();
 
 		string[] line = readSettings ();
-		sound.isOn = bool.Parse (line[0]);
+		//sound.isOn = bool.Parse (line[0]);
 		font_size.text = line [1];
 		time.text = line [2];
 	}
@@ -30,7 +30,7 @@ public class SettingsManager : MonoBehaviour {
 	}
 
 	public void saveSettings() {
-		PreferencesManager.save(sound.isOn.ToString(), font_size.text, time.text);
+		PreferencesManager.save("True", font_size.text, time.text);
 		SceneManager.LoadScene("game_menu");
 	}
 
